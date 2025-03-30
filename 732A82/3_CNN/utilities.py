ec8df5f099fee2aa77ffdcd2e2b8a8f516d943b0
Xiaochen Liu <a1029634929@gmail.com>
2025-03-30 23:40:53 +0200

    modification

---
+++ 732A82/3_CNN/utilities.py
@@ -0,0 +1,145 @@
+# IMPORTS
+import tensorflow as tf
+import tf_keras as keras
+
+from tf_keras.models import Sequential, Model
+from tf_keras.layers import Input, Dense, BatchNormalization, Dropout, Conv2D, MaxPooling2D, Flatten
+from tf_keras.optimizers import SGD, Adam
+
+# Set seed from random number generator, for better comparisons
+import numpy as np
+from numpy.random import seed
+seed(123)
+
+import matplotlib.pyplot as plt
+
+# from ray import train
+
+# # Set seed from random number generator, for better comparisons
+# from numpy.random import seed
+# seed(123)
+
+# import matplotlib.pyplot as plt
+
+# define funstion that builds a CNN model
+def build_CNN(input_shape, loss, 
+                n_conv_layers:int=2, 
+                n_filters:int=16, 
+                n_dense_layers:int=0, 
+                n_nodes:int=50, 
+                use_dropout:bool=False, 
+                learning_rate:float=0.01, 
+                act_fun='sigmoid', 
+                optimizer:str='sgd',
+                print_summary:bool=False):
+    """
+    Builds a Convolutional Neural Network (CNN) model based on the provided parameters.
+    
+    Parameters:
+    input_shape (tuple): Shape of the input data (excluding batch size).
+    loss (tf_keras.losses): Loss function to use in the model.
+    n_conv_layers (int, optional): Number of convolutional layers in the model. Default is 2.
+    n_filters (int, optional): Number of filters in each convolutional layer. Default is 16.
+    n_dense_layers (int, optional): Number of dense layers in the model. Default is 0.
+    n_nodes (int, optional): Number of nodes in each dense layer. Default is 50.
+    use_dropout (bool, optional): Whether to use Dropout after each layer. Default is False.
+    learning_rate (float, optional): Learning rate for the optimizer. Default is 0.01.
+    act_fun (str, optional): Activation function to use in each layer. Default is 'sigmoid'.
+    optimizer (str, optional): Optimizer to use in the model. Default is SGD.
+    print_summary (bool, optional): Whether to print a summary of the model. Default is False.
+    
+    Returns:
+    model (Sequential): Compiled Keras Sequential model.
+    """
+
+    # --------------------------------------------
+    # === Your code here =========================
+    # --------------------------------------------  
+
+    # Setup optimizer, depending on input parameter string
+    if optimizer == 'adam':
+        optimizer = Adam(learning_rate=learning_rate)
+    else: 
+        optimizer = SGD(learning_rate=learning_rate)   
+    # ============================================
+    
+    # Setup a sequential model
+    model = Sequential()
+
+    # --------------------------------------------
+    # === Your code here =========================
+    # --------------------------------------------  
+    
+    # Add convolutional layers
+    for i in range(n_conv_layers):
+        model.add(Conv2D(filters= n_filters*2**i, kernel_size=(3,3),
+                                padding="same",activation= act_fun, 
+                                input_shape=input_shape))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+    # Flatten the output of the convolutional layers
+    model.add(Flatten())     # 展平层，把特征图转换为一维
+    # Add dense layers     # 全连接层
+    for i in range(n_dense_layers):
+        model.add(Dense(n_nodes, activation= 'relu'))
+        model.add(BatchNormalization())
+        if use_dropout:
+            model.add(Dropout(0.5))
+    # Add output layer
+    model.add(Dense(10, activation='softmax'))
+    # Compile the model
+    model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+
+    # ============================================
+
+    # Print model summary if requested
+    if print_summary:
+        model.summary()
+    return model
+
+# =======================================
+# PLOTTING FUNCTIONS
+# =======================================
+
+# TRAINING CURVES PLOT FUNCTION
+def plot_results(history):
+    """
+    Plots the training and validation loss and accuracy from a Keras history object.
+    Parameters:
+    history (keras.callbacks.History): A History object returned by the fit method of a Keras model. 
+                                       It contains the training and validation loss and accuracy for each epoch.
+    Returns:
+    None
+    """
+    
+    val_loss = history.history['val_loss']
+    acc = history.history['accuracy']
+    loss = history.history['loss']
+    val_acc = history.history['val_accuracy']
+    
+    plt.figure(figsize=(10,4))
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.plot(loss)
+    plt.plot(val_loss)
+    plt.legend(['Training','Validation'])
+
+    plt.figure(figsize=(10,4))
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.plot(acc)
+    plt.plot(val_acc)
+    plt.legend(['Training','Validation'])
+
+    plt.show()
+
+
+# =======================================
+# AUGMENTATIONS FUNCTIONS
+# =======================================
+
+# ROTATE IMAGES BY () DEGREES
+def myrotate(images):
+
+    images_rot = tf.image.rot90(images, k=1)
+    
+    return images_rot
